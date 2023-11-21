f3be2de8dcaf6f1ede6d512247e85d183d9441f1
M Fahrur Rizky <fahrurrizky022@gmail.com>
2023-11-21 14:04:42 +0700

    test

---
+++ src/services/todo.js
@@ -0,0 +1,10 @@
+import { baseApi } from '@/plugins/axios'
+
+const api = '/api/todo'
+
+const list = () => baseApi.get(`${api}`)
+const add = (body) => baseApi.post(`${api}`, body)
+const remove = (id) => baseApi.delete(`${api}/${id}`)
+const edit = (id, body) => baseApi.put(`${api}/${id}`, body)
+
+export { list, add, remove, edit }
